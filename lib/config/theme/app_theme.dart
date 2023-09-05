d719cc5296e674c022e42e7a50d8bb29c739ffbd
Miguel Acosta <migue.un.av@gmail.com>
2023-09-05 02:28:07 -0600

    lista de items

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: constant_identifier_names
+
 import 'package:flutter/material.dart';
 
 class AppTheme {
@@ -8,3 +10,6 @@ class AppTheme {
 }
 
 const WHITE_SOLID = Color(0xFFFFFFFF);
+const BLACK_SOLID = Color(0xFF000000);
+const BLACK_LIGHT = Color(0x1F000000);
+const LIGHT_GRAY = Color(0x22616C93);
--- lib/presentation/widgets/share/item_file.dart
+++ lib/presentation/widgets/share/item_file.dart
@@ -1,40 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:ejercicio_files/config/theme/app_theme.dart';
 import 'package:ejercicio_files/domain/file_entity.dart';
 import 'package:ejercicio_files/domain/file_type.dart';
-import 'package:flutter/cupertino.dart';
 
 class ItemFile extends StatelessWidget {
-  FileEntity file;
+  final FileEntity file;
 
-  ItemFile({super.key, required this.file});
+  const ItemFile({super.key, required this.file});
 
   String _image() {
     switch (file.type) {
       case FileType.image:
-        return '';
+        return 'assets/icons/image.png';
       case FileType.pdf:
-        return '';
+        return 'assets/icons/pdf.png';
+      case FileType.video:
+        return 'assets/icons/video.png';
+      case FileType.doc:
+        return 'assets/icons/doc.png';
+      case FileType.audio:
+        return 'assets/icons/audio.png';
+      case FileType.other:
       default:
-        return '';
+        return 'assets/icons/image.png';
     }
   }
+
+  Widget _iconSource() => Image.asset(_image());
+
+  Widget _name() => Center(
+        child: Text(
+          file.name,
+          style: const TextStyle(
+            fontFamily: 'Roboto',
+            color: BLACK_SOLID,
+          ),
+        ),
+      );
+
+  Widget _delete() => SizedBox(
+      height: 40,
+      child: IconButton(
+        onPressed: () {},
+        icon: Image.asset('assets/icons/trash.png'),
+      ));
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.only(top: 20),
-      child: const SizedBox(
-        height: 60,
-        child: Row(
-          children: [
-            Expanded(
-              child: Placeholder(),
-              flex: 1,
-            ),
-            Expanded(
-              child: Placeholder(),
-              flex: 3,
-            )
-          ],
+      padding: const EdgeInsets.only(top: 15),
+      child: SizedBox(
+        height: 80,
+        child: Card(
+          color: BLACK_LIGHT,
+          child: Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: _iconSource(),
+              ),
+              Expanded(
+                flex: 3,
+                child: _name(),
+              ),
+              Expanded(
+                flex: 1,
+                child: _delete(),
+              )
+            ],
+          ),
         ),
       ),
     );
