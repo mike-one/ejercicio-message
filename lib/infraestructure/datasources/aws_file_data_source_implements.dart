fcbdbae9fa7ac0710571887990bf20047285955e
Miguel Acosta <migue.un.av@gmail.com>
2023-09-06 01:59:03 -0600

    Carga de archivos exitosa s3. Estado del boton

---
+++ lib/infraestructure/datasources/aws_file_data_source_implements.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:aws_s3_upload/aws_s3_upload.dart';
+import 'package:ejercicio_files/domain/datasources/file_datasource.dart';
+import 'package:ejercicio_files/domain/entities/file_entity.dart';
+
+class AWSFileDataSourceImplements extends FileDatasource {
+  @override
+  Future<FileEntity> getNewFile() {
+    // TODO: implement getNewFile
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<String?>> setNewFiles(List<FileEntity> files) async {
+    try {
+      final urls = await Future.wait(files.map((file) async {
+        return await AwsS3.uploadFile(
+          accessKey: dotenv.get('ACCESSKEY_AWS', fallback: 'not Found'),
+          secretKey: dotenv.get('SECRETKEY_AWS', fallback: 'not Found'),
+          file: File(file.path),
+          filename: file.name,
+          bucket: dotenv.get('BUCKET_NAME', fallback: 'not Found'),
+          region: dotenv.get('AWS_REGION', fallback: 'not Found'),
+        );
+      }).toList());
+      return urls;
+    } catch (error) {
+      print('Errorrrr:$error');
+      return [];
+    }
+  }
+}
